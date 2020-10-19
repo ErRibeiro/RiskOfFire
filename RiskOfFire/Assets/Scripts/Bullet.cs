2dfb41c7b590f0664e22f0ca1c8e64b94a104194
Eric Ribeiro <eric.ribeiro@live.com>
2020-10-19 01:46:19 -0500

    Added a player capsle, basic controls and a small gun(work in progress)

---
+++ RiskOfFire/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float speed = 5;
+
+    private float BulletLife = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+        BulletLife -= Time.deltaTime;
+        if (BulletLife <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ RiskOfFire/Assets/Scripts/Pistol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject BulletP;
+    [SerializeField]
+    public float cooldownTime;
+    public float coolDown = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cooldownTime > 0)
+        {
+            cooldownTime -= Time.deltaTime;
+        }
+        if (cooldownTime < 0)
+        {
+            cooldownTime = 0;
+        }
+        if (Input.GetMouseButton(0) && cooldownTime == 0)
+        {
+            GameObject bullet = Instantiate(BulletP);
+           
+            cooldownTime = coolDown;
+        }
+    }
+}
+
+    
+
+   
+
