7f1d3c6b9e0c657b75aa632223cbc74b5b5a1789
Eric Ribeiro <eric.ribeiro@live.com>
2020-10-19 13:13:03 -0500

    Added a powerup(item) that increases atks speed

---
+++ Assets/Scripts/FasterAtkSpeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FasterAtkSpeed : MonoBehaviour
+{
+    
+    private GameObject Pistol;
+    [SerializeField]
+    private GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(this.gameObject);
+            float cd;
+            cd = Pistol.GetComponent<Pistol>().coolDown;
+            cd = cd * .9f;
+            Pistol.GetComponent<Pistol>().coolDown = cd;
+        }   
+    }
+}
